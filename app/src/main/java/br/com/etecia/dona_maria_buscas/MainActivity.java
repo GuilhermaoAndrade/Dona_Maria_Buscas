62fea25c8f206086e6c45459886255ed1fce5b0e
Guilhermaoandrade <guilherme.souza102@fatec.sp.gov.br>
2019-09-11 18:46:08 -0300

    GridLayout adicionado

--- app/src/main/java/br/com/etecia/dona_maria_buscas/MainActivity.java
+++ app/src/main/java/br/com/etecia/dona_maria_buscas/MainActivity.java
@@ -1,13 +1,71 @@
 package br.com.etecia.dona_maria_buscas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    GridView gridView;
 
+    String[] fruitNames = {"Alimentos Basicos","Leite","Verduras","Frutas","Bebida Alcoolica","Chocolate"};
+    int[] fruitImages = {R.drawable.alimentosbasico,R.drawable.leite,R.drawable.verdura,R.drawable.fruta,R.drawable.bebidaalcolica,R.drawable.chocolate};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridview);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+
+        gridView.setAdapter(customAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+
+                Intent intent = new Intent(getApplicationContext(),GridItemActivity.class);
+                intent.putExtra("name",fruitNames[i]);
+                intent.putExtra("image",fruitImages[i]);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return fruitImages.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.row_data,null);
+
+            TextView name = view1.findViewById(R.id.fruits);
+            ImageView image = view1.findViewById(R.id.images);
+
+            name.setText(fruitNames[i]);
+            image.setImageResource(fruitImages[i]);
+            return view1;
+
+
+        }
     }
 }

9063a2d973f7b05996381ad46729dfb28503caac
Guilhermaoandrade <guilherme.souza102@fatec.sp.gov.br>
2019-09-17 21:28:09 -0300

    Alteracao no cadastro

--- app/src/main/java/br/com/etecia/dona_maria_buscas/CadastroActivity.java
+++ app/src/main/java/br/com/etecia/dona_maria_buscas/CadastroActivity.java
@@ -2,12 +2,31 @@ package br.com.etecia.dona_maria_buscas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CadastroActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_layout);
+        Spinner spinner = findViewById(R.id.spinnerUF);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.lista_uf, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_LONG).show();
     }
 }
